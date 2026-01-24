d16b7a08fbf85c89b6cc8019b7a921079d4a6d72
Andreas Hocevar <andreas.hocevar@gmail.com>
2026-01-24 11:18:21 +0100

    Favicon and app title

--- src/composables/useMap.ts
+++ src/composables/useMap.ts
@@ -2,47 +2,19 @@ import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import Group from 'ol/layer/Group';
+import { defaults as defaultControls } from 'ol/control';
 import { apply } from 'ol-mapbox-style';
 import { register as registerPMTilesProtocol } from 'pmtiles-protocol';
 
 registerPMTilesProtocol();
 
 const mgrs = new Group();
-apply(mgrs, {
-  version: 8,
-  name: 'MGRS Graticule',
-  sources: {
-    test: {
-      type: 'raster',
-      url: 'pmtiles://https://pmtiles.io/stamen_toner(raster)CC-BY+ODbL_z3.pmtiles',
-    },
-    mgrs: {
-      type: 'vector',
-      url: 'pmtiles://https://data.source.coop/ftw/ftw-grid/ftw_grid.pmtiles',
-    },
-  },
-  layers: [
-    {
-      id: 'test',
-      type: 'raster',
-      source: 'test',
-    },
-    {
-      id: 'mgrs',
-      type: 'fill',
-      source: 'mgrs',
-      'source-layer': 'ftw_grid4',
-      paint: {
-        'fill-color': 'rgba(255, 0, 0, 0.1)',
-        'fill-outline-color': 'rgba(255, 0, 0, 0.5)',
-      },
-    },
-  ],
-});
+apply(mgrs, './style.json');
 
 let map: Map;
 function createMapInstance() {
   return new Map({
+    controls: defaultControls({ attributionOptions: { collapsible: false } }),
     layers: [mgrs],
     view: new View({
       center: [0, 0],
